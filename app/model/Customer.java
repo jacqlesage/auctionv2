392019fcf9e6f15d6668c8769250fe353170036c
james <jacqlesages@gmail.com>
2017-10-03 21:27:26 +1300

    Just sorting out a customer table. I have my first DB evolution. I have some of the data coming through the customer model but it is not showing the password which I think is due to the value being transient prior. I will check up on all this tomorrow - might just need a reset.

--- app/model/Customer.java
+++ app/model/Customer.java
@@ -22,64 +22,62 @@ public class Customer extends Model{
     @Id
     public int id;
 
-     @Constraints.Required(message="validation.required")
+    // @Constraints.Required(message="validation.required")
 //    @Constraints.Email(message="validation.fname")
     @Constraints.MaxLength(value=100,message="validation.maxLength")
 //    @Column(unique=true, nullable=false, length=100)
     public String firstName;
 
-    @Constraints.Required(message="validation.required")
+   // @Constraints.Required(message="validation.required")
 //    @Constraints.Email(message="validation.lname")
     @Constraints.MaxLength(value=100,message="validation.maxLength")
 //    @Column(unique=true, nullable=false, length=100)
     public String lastName;
-
-    @Constraints.MaxLength(value=20,message="validation.maxLength")
-    @Constraints.Required(message="validation.required")
+//
+//    @Constraints.MaxLength(value=20,message="validation.maxLength")
+//    @Constraints.Required(message="validation.required")
     public String phoneNumber;
 
     //    @Column(unique=true, nullable=false, length=100)
-    @Constraints.Required(message="validation.required")
-    @Constraints.Email(message="validation.email")
+    //@Constraints.Required(message="validation.required")
+    //@Constraints.Email(message="validation.email")
     @Constraints.MaxLength(value=100,message="validation.maxLength")
-
     public String email;
 
     //    address details - Better to use an address finder as opposed to regex as the pattern would be consistant
     //eg Address validation using Google Maps API or nz post code finder
-    @Constraints.Required(message="validation.required")
+    //@Constraints.Required(message="validation.required")
     public String address1;
 
     public String address2;
 
 //    @Min(3)
-    @Constraints.Required(message="validation.required")
+   // @Constraints.Required(message="validation.required")
     public String suburb;
 
 //    @Min(3)
-    @Constraints.Required(message="validation.required")
+    //@Constraints.Required(message="validation.required")
     public String city;
 
 //    @Min(4)
 ////  @Max(6) this will also be directed by the address checker.
-    @Constraints.Required(message="validation.required")
+    //@Constraints.Required(message="validation.required")
     public int postcode;
 
     //can also use a country selector here to clean up entires into the DB.
-    @Constraints.Required(message="validation.required")
+    //@Constraints.Required(message="validation.required")
     public String country;
-
-
-    @JoinColumn(table="cusotmer_login")
-    @Constraints.Required(message="validation.required")
-    @Transient //Defines this field as being transient (not persisted)
-    @JsonIgnore
+    
+    //@JoinColumn(table="cusotmer_login")
+    //@Constraints.Required(message="validation.required")
+    //@Transient //Defines this field as being transient (not persisted)
+    //@JsonIgnore
     public String password;
 
     //this should be auto to 1 once email validation is done.- needs some work on this.
 //    @Constraints.Min(1)
 //    @Constraints.MaxLength(1)
-     @Constraints.Required(message="validation.required")
+     //@Constraints.Required(message="validation.required")
      public int active;
 
 //    @OneToOne(mappedBy = "customer")
@@ -87,7 +85,7 @@ public class Customer extends Model{
 //    @JsonIgnore
 //     public CustomerLogin customerLogin;
 
-    @JsonIgnore
+    //@JsonIgnore
     @Transient
     public Finder<Integer, Customer> find = new Finder<Integer, Customer>(Customer.class);
 
@@ -112,6 +110,16 @@ public class Customer extends Model{
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+
+    }
+
+    public Customer(String firstName, String lastName, String email, String password) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+
     }
 
     public Customer() {
