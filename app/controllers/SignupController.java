3e01e0d4b2a1f98a659066bcc7a76d955d4aec7e
james <jacqlesages@gmail.com>
2017-10-04 19:37:07 +1300

    making signup controller

---
+++ app/controllers/SignupController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import model.Customer;
+import play.data.Form;
+import play.mvc.*;
+import play.data.FormFactory;
+
+import javax.inject.Inject;
+
+/**
+ * Created by james on 3/10/17.
+ */
+public class SignupController extends Controller{
+
+    @Inject
+    FormFactory formFactory;
+
+//    @Inject
+//    public SignupController(FormFactory formFactory) {
+//        this.signupForm =  formFactory.form(Customer.class).bindFromRequest().get();;
+//
+//    }
+
+
+
+
+    public Result signup() {
+        //Form<Customer> userForm = formFactory.form(Customer.class);
+        Customer customer = formFactory.form(Customer.class).bindFromRequest().get();
+
+        if(customer.findCustomer(customer.email)){
+
+            //then go back to page and advise email in use
+        }else{
+            //email is not in use proceed with adding customer.
+        }
+
+        //first check that customer has not sigend up
+
+        return ok(customer.toString());
+    }
+}
