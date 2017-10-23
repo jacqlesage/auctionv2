a6ea83d9a13338170b049868249b372c2b4c93ab
james <jacqlesages@gmail.com>
2017-10-23 16:00:43 +1300

    Ajax signup working

--- public/javascripts/confirm_password.js
+++ public/javascripts/confirm_password.js
@@ -23,7 +23,7 @@ function validatePassword(){
         //$('submitBtn').click(function(evt) {
             console.log("Success!11111");
 
-
+            var toPostorNot;
 
 
             var obj = {
@@ -37,32 +37,86 @@ function validatePassword(){
 
         console.log(JSON.stringify(obj));
 
-
-
-
-        $.ajax({
-            url: "@routes.SignupController.signup",
+          $.ajax({
+            type :  "GET",
+            dataType: 'json',
             data: JSON.stringify(obj),
-            headers: {
-                'Content-Type': 'application/json'
-         },
-            type: 'POST',
-            success: function(res) {
-                if (res) {
-                    console.log(obj);
+            contentType: "application/json; charset=utf-8",
+            url  :  "/customer/"+ obj.email,
+            success: function(data) {
+               alert("data.reponse = " +data.toString() + " " +" obj.email = " + obj.email.toString() );
 
-                } else {
-                    console.log("Failed...");
-                    alert("fail");
+                if(obj.email.toString() != data.toString()){
+
+                    
+
+                    alert("email  the same");
+
+
+                }else{
+
+                    $.ajax({
+                        type :  "POST",
+                        dataType: 'json',
+                        data: JSON.stringify(obj),
+                        contentType: "application/json; charset=utf-8",
+                        url  :  "/jsRoutes"
+
+                });
+
+
+                }}});
+                    // var r = jsRoutes.controllers.SignupController.signup;
+                    // $.ajax(jsRoutes.controllers.SignupController.signup);
+
+                    // $.ajax({
+                    //     type :  "POST",
+                    //     url: "/jsRoutes",
+                    //     data: obj,
+                    //     dataType: "json",
+                    //     contentType: "application/json",
+                    //     success: function (msg) {
+                    //         alert(msg.success)
+                    //     },
+                    //     error: function () {
+                    //         alert("error");
+                    //
+                    //     }
+                    // });
+
+                    // {url: r.url, type: r.type, success: /*...*/, error: /*...*/ }
+                    //
+                    //  $.ajax("routes.SignupController.signup")
+                    //      // .ajaxSuccess(console.log("ajaxsucess"))
+                    //      .done( console.log("failed here1") )
+                    //      .fail( console.log("failed here2"));
+
+
+                    //
+                    // $.ajax({
+                    //     url: "@routes.SignupController.signup",
+                    //     data: JSON.stringify(obj),
+                    //     headers: {
+                    //         'Content-Type': 'application/json'
+                    //  },
+                    //     type: 'POST',
+                    //     success: function(res) {
+                    //         if (res) {
+                    //             console.log(obj);
+                    //             console.log("Failed...");
+                    //
+                    //         } else {
+                    //             console.log("Failed...");
+                    //             alert("fail");
+                    //         }
+                    //
+                    //     }});
+
+                    //document.forms['signupForm'].submit();
+                    //return true;
+
+
                 }
-
-            }});
-    }
-        //document.forms['signupForm'].submit();
-        //return true;
-
-
-
-};
+            }
 // password.onchange = validatePassword;
 // confirm_password.onkeyup = validatePassword;
