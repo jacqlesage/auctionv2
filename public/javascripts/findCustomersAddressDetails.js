3ca3860ca1cfa60054d727f332d5b567f1e7688a
james <jacqlesages@gmail.com>
2017-12-31 17:39:38 +1300

    Finally getting the javascript router to work. Now - just need to figure out how to use it. (Seems like the issue was more in the ide than play, but an update and the use of activator instead of SBT changed things - not sure what excatley however)

---
+++ public/javascripts/findCustomersAddressDetails.js
@@ -0,0 +1,29 @@
+
+
+alert("test");
+
+$(document).ready(function(){
+    $("#details").click(function(){
+        alert("testsss");
+    });
+});
+// var emailFromDom = document.getElementById("emailFromDashboard");
+//
+// $('#myAddressDetails1').click(function(){
+//     alert("test");
+//     $.ajax(jsRoutes.controllers.JsController.getUserAddressDetails())
+//         .done(function (data) {
+//             alert(data);
+//             console.log("this is a console log " + data)
+//             console.log("this is a console log " + data)
+//             var addressLine1 = document.getElementById("address1");
+//             var addressLine2 = document.getElementById("address2");
+//             var city = document.getElementById("city");
+//             var country = document.getElementById("country");
+//             var postCode = document.getElementById("postCode");
+//
+//
+//         }).fail(function (data) {
+//         alert("Try again champ!");
+//     });
+// }
