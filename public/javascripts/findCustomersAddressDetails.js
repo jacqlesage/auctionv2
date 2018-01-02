fb6faf1c5d3ef0df64f357a2baa6cf52ec0da1ee
james <jacqlesages@gmail.com>
2018-01-02 19:14:42 +1300

    jsRoutes seems to be working now - bit of messing around wiuth it for sure - should be helpfull as from here on in.

--- public/javascripts/findCustomersAddressDetails.js
+++ public/javascripts/findCustomersAddressDetails.js
@@ -1,15 +1,35 @@
 
 
-alert("test");
+
+
 
 $(document).ready(function(){
+
+
     $("#details").click(function(){
-        alert("testsss");
+        var emailFromDashboard = document.getElementById("emailFromDashboard").value;
+        console.log(emailFromDashboard.toString());
+        alert(emailFromDashboard.toString());
+        $.ajax(jsRoutes.controllers.JsController.findCustomerByEmail(emailFromDashboard))
+            .done(function (data) {
+                alert(data);
+                console.log("this is a console log " + data);
+                console.log("this is a console log " + data);
+                var addressLine1 = document.getElementById("address1");
+                var addressLine2 = document.getElementById("address2");
+                var city = document.getElementById("city");
+                var country = document.getElementById("country");
+                var postCode = document.getElementById("postCode");
+
+
+            }).fail(function (data) {
+            alert("Try again champ!");
+        });
     });
 });
-// var emailFromDom = document.getElementById("emailFromDashboard");
+var emailFromDom = document.getElementById("emailFromDashboard");
 //
-// $('#myAddressDetails1').click(function(){
+// $('#details').click(function(){
 //     alert("test");
 //     $.ajax(jsRoutes.controllers.JsController.getUserAddressDetails())
 //         .done(function (data) {
