fd382e55a0f93cbe3487f2a783484d2db7224747
james <jacqlesages@gmail.com>
2018-01-06 13:14:08 +1300

    jsRoutes seems to be working now - bit of messing around wiuth it for sure - just sorting out the ajax on it

--- public/javascripts/findCustomersAddressDetails.js
+++ public/javascripts/findCustomersAddressDetails.js
@@ -3,31 +3,49 @@
 
 
 
-$(document).ready(function(){
-
-
+$(document).ready(function() {
+    var emailFromDashboard = document.getElementById("emailFromDashboard").value;
+    alert(emailFromDashboard);
     $("#details").click(function(){
-        var emailFromDashboard = document.getElementById("emailFromDashboard").value;
-        console.log(emailFromDashboard.toString());
-        alert(emailFromDashboard.toString());
-        $.ajax(jsRoutes.controllers.JsController.findCustomerByEmail(emailFromDashboard))
-            .done(function (data) {
-                alert(data);
-                console.log("this is a console log " + data);
-                console.log("this is a console log " + data);
-                var addressLine1 = document.getElementById("address1");
-                var addressLine2 = document.getElementById("address2");
-                var city = document.getElementById("city");
-                var country = document.getElementById("country");
-                var postCode = document.getElementById("postCode");
 
+    var r = jsRoutes.controllers.JsController.findCustomerByEmail(emailFromDashboard);
+    $.ajax({
+        url: r.url, type: r.type, dataType: "JSON",
+        success: (function (data) {
+            alert("success!");
+            alert(data.toString());
+            console.log("this is a console log " + data.toString());
+        }),
 
-            }).fail(function (data) {
+        error: (function (data) {
             alert("Try again champ!");
-        });
-    });
+        })
+    })
+})
 });
-var emailFromDom = document.getElementById("emailFromDashboard");
+//
+//     $("#details").click(function(){
+//         var emailFromDashboard = document.getElementById("emailFromDashboard").value;
+//         console.log(emailFromDashboard.toString());
+//         alert(emailFromDashboard.toString());
+//         $.ajax(jsRoutes.controllers.JsController.findCustomerByEmail(emailFromDashboard))
+//             .done(function (data) {
+//                 alert(data);
+//                 console.log("this is a console log " + data.toString());
+//                 console.log("this is a console log " + data);
+//                 var addressLine1 = document.getElementById("address1");
+//                 var addressLine2 = document.getElementById("address2");
+//                 var city = document.getElementById("city");
+//                 var country = document.getElementById("country");
+//                 var postCode = document.getElementById("postCode");
+//
+//
+//             }).fail(function (data) {
+//             alert("Try again champ!");
+//         });
+//     });
+// });
+// var emailFromDom = document.getElementById("emailFromDashboard");
 //
 // $('#details').click(function(){
 //     alert("test");
