9673e302bf0a000c49c11b318807d90ee290c39e
james <jacqlesages@gmail.com>
2018-02-05 22:25:05 +1300

    just working on log in as I am sick of working on the tables at this point :)

---
+++ public/javascripts/confirmAndLoginCustomer.js
@@ -0,0 +1,44 @@
+
+$(document).ready(function() {
+
+  $("#submitBtnFromLogin").click(function(){
+
+      var emailFromLogin = document.getElementById("emailFromLogin").value;
+
+      var passwordFromLogin = document.getElementById("passwordFromLogin").value;
+
+      if(passwordFromLogin.length == 0 || emailFromLogin.length == 0) {
+            alert("it is empty");
+          //emailFromLogin[0].style.borderColor = "red";
+          //emailFromLogin.setCustomValidity('Please confirm your email');
+
+          //passwordFromLogin.style.borderColor = "red";
+          //passwordFromLogin.setCustomValidity('Please confirm your password');
+      }else {
+
+          var r = jsRoutes.controllers.JsController.findCustomerByEmail(emailFromLogin);
+          $.ajax({
+              url: r.url, type: r.type, dataType: "JSON",
+              success: (function (data) {
+
+                  console.log(data);
+
+                  if (data.phoneNumber == null) {
+                      //only need to update phone here as the rest of the details are puled from session...humm....
+                      phone.value = "Please update";
+                      //password.value = "You can change your password here";
+
+                  } else {
+                      //password.value = "You can change your password here";
+
+                  }
+
+              }),
+
+              error: (function (data) {
+                  alert("Try again champ!");
+              })
+          })
+      }
+    })
+});
