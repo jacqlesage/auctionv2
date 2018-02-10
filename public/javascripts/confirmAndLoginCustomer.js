75dba9ca24214f054ac2c20484f47ef64554d89f
james <jacqlesages@gmail.com>
2018-02-10 19:35:31 +1300

    just working on log in as I am sick of working on the tables at this point :)

--- public/javascripts/confirmAndLoginCustomer.js
+++ public/javascripts/confirmAndLoginCustomer.js
@@ -32,26 +32,29 @@ $(document).ready(function() {
       }else {
 
           var r = jsRoutes.controllers.JsController.findCustomerByEmail(emailFromLogin);
+          var r = jsRoutes.controllers.JsController.login(emailFromLogin);
           $.ajax({
               url: r.url, type: r.type, dataType: "JSON",
               success: (function (data) {
 
                   console.log(data);
-
-                  if (data.phoneNumber == null) {
-                      //only need to update phone here as the rest of the details are puled from session...humm....
-                      phone.value = "Please update";
-                      //password.value = "You can change your password here";
-
-                  } else {
-                      //password.value = "You can change your password here";
-
-                  }
+                  //
+                  // if (data.firstName == null) {
+                  //     //Then we know there is no customer found
+                  //     emailFromLoginInputBox.setCustomValidity('Please register this email');
+                  //     //password.value = "You can change your password here";
+                  //
+                  // } else {
+                  //     //password.value = "You can change your password here";
+                  //
+                  // }
 
               }),
 
               error: (function (data) {
-                  alert("Try again champ!");
+                  emailFromLoginInputBox.setCustomValidity("Please register this email");
+                      emailFromLoginInputBox.style.borderColor = "blue";
+
               })
           })
       }
