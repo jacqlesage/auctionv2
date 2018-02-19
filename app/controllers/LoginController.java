17e59c5e5035b0ab343d75c2f412eeff17a9e541
james <jacqlesages@gmail.com>
2018-02-19 22:52:47 +1300

    Found out poast data needs this setup Map<String, String[]> form_values = request().body().asFormUrlEncoded(); to find the bind? Not sure why yet - I can see what I need to be able to complete the login and direct the customer to the bidding page.

--- app/controllers/LoginController.java
+++ app/controllers/LoginController.java
@@ -8,6 +8,7 @@ import play.mvc.*;
 import play.data.FormFactory;
 
 import javax.inject.Inject;
+import java.util.Map;
 
 /**
  * Created by james on 29/10/17.
@@ -17,12 +18,18 @@ LoginController extends Controller {
 
     @Inject
     FormFactory formFactory;
-
+    //Post data seems to be needing this sort of form setup to get it ? Map<String, String[]> form_values = request().body().asFormUrlEncoded();
+    //not fully sure why as yet - I suspect that I not yet fully using Play froms in 2.6 to their fullest.
     public Result login() {
-        //Form<Customer> userForm = formFactory.form(Customer.class);
-        Customer customer = formFactory.form(Customer.class).bindFromRequest().get();
-        System.out.print(customer.toString());
 
+        //System.out.println(userForm.toString() + "@@@!@!@!@!@!@");
+        Customer customer = formFactory.form(Customer.class).bindFromRequest().get();
+        Map<String, String[]> form_values = request().body().asFormUrlEncoded();
+        System.out.println(customer.email+ " ");
+        System.out.println(customer.password + " ");
+        System.out.println("!!!!" + form_values.get("emailFromLogin")[0]);
+        System.out.println("!!!!" + form_values.get("passwordFromLogin")[0]);
+        System.out.println("!!!!" + form_values.get("test")[2]);
         customer = Customer.findCustomerByEmail(customer.email);
         System.out.print(customer.toString() + " ");
         //check the password
