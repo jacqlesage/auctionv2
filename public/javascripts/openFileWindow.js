5cdc89474fd1409c2e7d5df13d65126140f028cd
james <jacqlesages@gmail.com>
2018-03-23 16:34:27 +1300

    got the full file path now and have been able to input vvalues back into the forms for sending.

--- public/javascripts/openFileWindow.js
+++ public/javascripts/openFileWindow.js
@@ -1,10 +1,46 @@
 
-function copyFileValueAcrossToDisplay(){
-    alert("tests");
+$(function() {
+    $("#addFileBtn1").click(function () {
+        alert("test23#");
+        var r =  jsRoutes.controllers.HomeController.uploadFile();
+        $.ajax({
+            url: r.url, type: r.type, dataType: "JSON",
+            success: (function (data) {
 
-    var x = document.getElementById("file").value;
-    x.value = fileInput.value.replace("C:\\fakepath\\", "");
-    document.getElementById("auctionVideo").value = x;
+                console.log(data);
+                document.getElementById("auctionVideo").value = data.toString();
 
 
-};
+            }),
+
+            error: (function (data) {
+
+
+                alert("no file selected");
+            })
+        })
+    })
+});
+
+$(function() {
+    $("#addFileBtn2").click(function () {
+        alert("test24#");
+        var r =  jsRoutes.controllers.HomeController.uploadFile();
+        $.ajax({
+            url: r.url, type: r.type, dataType: "JSON",
+            success: (function (data) {
+
+                console.log(data);
+                document.getElementById("auctionMainPicture").value = data.toString();
+
+
+            }),
+
+            error: (function (data) {
+
+
+                alert("no file selected");
+            })
+        })
+    })
+});
