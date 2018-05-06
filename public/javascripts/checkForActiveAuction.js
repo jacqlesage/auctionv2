52ee385715e8e0cd60df0ef555722cfda240c1f6
james <jacqlesages@gmail.com>
2018-05-06 21:43:33 +1200

    Got Ajax working so now I can fill the auction field up with values, vids etc from the saved "active" auction. Once code is in place for that it will be time to tidy up the first half of the code as the next step is building the dash board and bidding on auctions etc.

--- public/javascripts/checkForActiveAuction.js
+++ public/javascripts/checkForActiveAuction.js
@@ -1,16 +1,16 @@
-alert("testingFindAuction");
 
 $(function() {
 
         alert("test25#");
-    alert("fishsihs");
+
         var r =  jsRoutes.controllers.JsController.findActiveAuction();
-        alert("fishsihsxxx");
+
         $.ajax({
             url: r.url, type: r.type, dataType: "JSON",
             success: (function (data) {
 
-                console.log(data + " success from getActive");
+                console.log(data.id.toString() + " success from getActive");
+                console.log(" " + JSON.stringify(data) +" success from getActive");
                 //document.getElementById("auctionMainPicture").value = data.toString();
 
 
@@ -18,8 +18,8 @@ $(function() {
 
             error: (function (data) {
 
-                console.log(data.toString() + " NON success from getActive");
-                alert("no success from getActvie");
+                console.log(data.toString() + " " + JSON.stringify(data) +" NON success from getActive");
+                alert(JSON.stringify(data));
             })
         });
 
@@ -29,4 +29,36 @@ $(function() {
 // $.ajax(jsRoutes.controllers.JsController.findActiveAuction())
 //     .done( alert("TestSuccess") )
 //     .fail( alert("TestFail"));
+// });
+
+// var r = jsRoutes.controllers.JsController.findCustomerByEmail(emailFromDashboard);
+// $.ajax({
+//     url: r.url, type: r.type, dataType: "JSON",
+//     success: (function (data) {
+//
+//         console.log(data);
+//
+//         if(data.address1 == null) {
+//             addressLine1.value = "Please update";
+//             addressLine2.value = "Please update";
+//             //phone.value = "Please update";
+//             city.value = "Please update";
+//             country.value = "Please update";
+//             postCode.value = "Please update";
+//
+//         }else{
+//             addressLine1.value = data.address1;
+//             addressLine2.value = data.address2;
+//             city.value = data.city;
+//             //phone.value = data.phone;
+//             country.value = data.country;
+//             postCode.value = data.postCode;
+//         }
+//     }),
+//
+//     error: (function (data) {
+//         alert("Try again champ!");
+//     })
+// })
+// })
 // });
