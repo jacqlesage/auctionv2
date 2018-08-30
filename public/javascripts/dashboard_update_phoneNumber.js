a143531aa0f174153f16c2409b7c5c0e7e41aa77
jacqlesage <jacqlesages@gmail.com>
2018-08-30 19:34:15 +1200

    checking if the phone number has changed if so then update DB.

---
+++ public/javascripts/dashboard_update_phoneNumber.js
@@ -0,0 +1,11 @@
+function updatePhoneNumber() {
+
+    //check if it has changed and not empty
+
+    //if empty then no change / ignore the click in the box basically
+
+    //if yes then make the change on the DB via ajax using controllers
+
+
+    
+}
