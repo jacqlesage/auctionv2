98dc3c1a3401a1dc29358c6b80be2fc5839897ef
jacqlesage <jacqlesages@gmail.com>
2020-03-04 20:58:13 +1300

    creating a seperate js for update of postal details
    Will be placing the project on hold while I build another suggestion which should be quicker to starting

--- app/model/PostalAddressDAO.java
+++ app/model/PostalAddressDAO.java
@@ -1,10 +1,12 @@
 package model;
 
+import io.ebean.Finder;
 import io.ebean.Model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 
 @Entity
@@ -23,6 +25,16 @@ public class PostalAddressDAO extends Model{
     public int id;
 
 
+    @Transient
+    public static Finder<Integer, PostalAddressDAO> find = new Finder<Integer, PostalAddressDAO>(PostalAddressDAO.class);
 
-
+    public PostalAddressDAO(String address1, String address2, String city, String country, String postCode, String cusEmailReference, int cusNumberReference) {
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.country = country;
+        this.postCode = postCode;
+        this.cusEmailReference = cusEmailReference;
+        this.cusNumberReference = cusNumberReference;
+    }
 }
